1818f63e416e902a17025493ccbd19fa816e4b61
Jake2000 <razumov.evgeni@yandex.ru>
2016-01-25 01:46:57 +0300

    Update ember-cli-build.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,18 +13,6 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
-  app.import('bower_components/kendo-ui/styles/kendo.common.core.min.css');
-  app.import('bower_components/kendo-ui/styles/kendo.common.min.css')
-  app.import('bower_components/kendo-ui/styles/kendo.common-material.core.min.css');
-  app.import('bower_components/kendo-ui/styles/kendo.common-material.min.css');
-  app.import('bower_components/kendo-ui/styles/kendo.material.min.css');
-
-  app.import('bower_components/kendo-ui/styles/Material/loading.gif', {
-    distDir: 'assets'
-  });
-  app.import('bower_components/kendo-ui/styles/Material/sprite.png', {
-    distDir: 'assets'
-  });
 
   return app.toTree();
 };
