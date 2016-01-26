c7bf1726dc25004584710185b5f4ba6324ddc94f
Jake2000 <razumov.evgeni@yandex.ru>
2016-01-26 19:28:29 +0300

    Update value.js

--- addon/mixins/value.js
+++ addon/mixins/value.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
+const {Mixin, observer, isEmpty} = Ember;
 
-let onChange = Ember.Mixin.create({
-  _onChange: Ember.observer('$object', function() {
+let onChange = Mixin.create({
+  _onChange: observer('$object', function() {
     let $object = this.get('$object');
-    if(!Ember.isEmpty($object)) {
+    if(!isEmpty($object)) {
       $object.bind('change', () => {
         this.set('value', $object.value());
       });
@@ -11,10 +12,10 @@ let onChange = Ember.Mixin.create({
   })
 });
 
-let onDataBound = Ember.Mixin.create({
-  _onDataBound: Ember.observer('$object', function() {
+let onDataBound = Mixin.create({
+  _onDataBound: observer('$object', function() {
     let $object = this.get('$object');
-    if(!Ember.isEmpty($object)) {
+    if(!isEmpty($object)) {
       $object.bind('dataBound', () => {
         this.set('value', $object.value());
       });
@@ -22,6 +23,26 @@ let onDataBound = Ember.Mixin.create({
   })
 });
 
-export {onChange, onDataBound};
+let onSelect = Mixin.create({
+  _onSelect: observer('$object', function() {
+    let $object = this.get('$object');
+    if(!isEmpty($object)) {
+      $object.bind('select', () => {
+        this.set('value', $object.value());
+      });
+    }
+  })
+});
 
-export default Ember.Mixin.create(onChange, onDataBound);
+let value = Mixin.create({
+  _value: observer('$object', 'value', function() {
+    let $object = this.get('$object'), value = this.get('value');
+    if(!isEmpty($object)) {
+      $object.value(value);
+    }
+  })
+});
+
+export {onChange, onDataBound, onSelect, value};
+
+export default Ember.Mixin.create(onChange, onDataBound, onSelect, value);
