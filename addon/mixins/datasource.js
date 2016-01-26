d7963ec565760377f6b2ebc1741287083e7f1f31
Jake2000 <razumov.evgeni@yandex.ru>
2016-01-26 19:21:16 +0300

    Update datasource.js

--- addon/mixins/datasource.js
+++ addon/mixins/datasource.js
@@ -4,7 +4,6 @@ import Serialize from './serialize';
 export default Ember.Mixin.create(Serialize, {
   _dataSource: Ember.observer('dataSource.[]', function() {
     let $object = this.get('$object'), dataSource = this._serialize(this.get('dataSource'));
-    dataSource = Array.isArray(dataSource) ? dataSource : [];
     if(!Ember.isEmpty($object)) {
       $object.setDataSource(dataSource);
     }
